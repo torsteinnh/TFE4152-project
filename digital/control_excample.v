3d17fd0c3f8f4ddafe322d6302ec28c99661454d
Torstein N-H <torsteinnh@gmail.com>
2019-09-19 16:15:29 +0200

    Start of project

---
+++ digital/control_excample.v
@@ -0,0 +1,18 @@
+//
+//
+// Top-level verilog module example
+//
+
+module re_control(Expose, Erase, ADC, NRE_1, NRE_2,
+	Init, Exp_increase, Exp_decrease, Reset, Clk);
+
+	input Init, Exp_increase, Exp_decrease, Reset, Clk;
+	output Expose, Erase, ADC, NRE_1, NRE_2;
+	
+	
+	//
+	// Your code goes here
+	//
+	
+endmodule // re_control
+
