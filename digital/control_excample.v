37a7945bf85747a70c7d0c7e41381d54d95ae50d
Torstein N-H <torsteinnh@gmail.com>
2019-10-21 16:17:26 +0200

    Penere figurer ol

--- digital/control_excample.v
+++ digital/control_excample.v
@@ -3,16 +3,15 @@
 // Top-level verilog module example
 //
 
-module re_control(Expose, Erase, ADC, NRE_1, NRE_2,
-	Init, Exp_increase, Exp_decrease, Reset, Clk);
+module control_excample(Expose, Erase, ADC, NRE_1, NRE_2, Init, Exp_increase, Exp_decrease, Reset, Clk);
 
-	input Init, Exp_increase, Exp_decrease, Reset, Clk;
-	output Expose, Erase, ADC, NRE_1, NRE_2;
-	
-	
-	//
-	// Your code goes here
-	//
-	
+   input Init, Exp_increase, Exp_decrease, Reset, Clk;
+   output Expose, Erase, ADC, NRE_1, NRE_2;
+   
+   
+   //
+   // Your code goes here
+   //
+   
 endmodule // re_control
 
