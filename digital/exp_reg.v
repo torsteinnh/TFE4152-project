937becea42988bc172960f942109b01c6eb33121
Torstein N-H <torsteinnh@gmail.com>
2019-10-28 17:57:53 +0100

    Mer veriloggit status!

--- digital/exp_reg.v
+++ digital/exp_reg.v
@@ -11,7 +11,7 @@ module exp_reg(input  logic clk, reset, inc, dec,
 	       output logic [4:0] q);
 
    always @(posedge clk)
-     if (reset)
+     if (reset | q > 30 | q < 2)
        q <= 5'd15;
      else if (inc & (q < 30))
        q <= q + 1;
@@ -23,6 +23,7 @@ endmodule // exp_reg
 
 `ifndef _no_testbench_
 
+
 module exp_reg_tb;
    logic clk, reset, inc, dec;
    logic [4:0] q;
@@ -30,7 +31,7 @@ module exp_reg_tb;
    exp_reg testreg(clk, reset, inc, dec, q);
 
    initial begin
-      $dumpfile("out_exp_reg_tb.vcd");
+      $dumpfile("outfiles/out_exp_reg_tb.vcd");
       $dumpvars();
 
       #1 {clk, reset, inc, dec} = 4'b1100;
@@ -39,16 +40,14 @@ module exp_reg_tb;
       for (int i = 1; i <= 20; i = i+1) begin
 	 #1 {clk, inc, dec} = 3'b111;
 	 #1 {clk, inc, dec} = 3'b011;
-	 $display("%d", i);
       end
 
-      for (int i = 1; i <= 40; i = i+1) begin
+      for (i = 1; i <= 40; i = i+1) begin
 	 #1 {clk, inc, dec} = 3'b101;
 	 #1 {clk, inc, dec} = 3'b001;
-	 $display("%d", i);
       end
 
-      #1 {clk, reset, inc, dec} = 4'b1100;
+      #1 {clk, reset, inc, dec} = 4'b1101;
       #1 {clk, reset} = 2'b00;
       #1 clk = 1;
       #1 clk = 0;
@@ -56,7 +55,6 @@ module exp_reg_tb;
       #1 $finish;
    end
    
-
 endmodule // exp_reg_tb
 
 
---
+++ digital/fcd_reg.v
@@ -0,0 +1,76 @@
+/*
+ This file contains the register used to count down different values in the camera.
+ It has 5 bits, can take data inn and counts down on rising clock edge when enabled.
+ It has an output for when the internal data is 0.
+ Data set has priority over enable.
+ */
+
+`ifndef _fcd_reg_v_28_
+ `define _fcd_reg_v_28_
+
+
+module fcd_reg(input logic clk, enable, dset,
+	       input logic [4:0] dread_data,
+	       output logic 	 finished);
+   
+   logic [4:0] 			 data_int;
+   
+   always @(posedge clk)
+     if (dset)
+       data_int <= dread_data;
+     else if (enable & data_int > 'b0)
+       data_int <= data_int - 1;
+
+   assign finished = (data_int == 'b0) ? 1 : 0;
+   
+endmodule // fcd_reg
+
+
+`ifndef _no_testbench_
+
+
+module fcd_reg_tb;
+   logic clk, enable, dset, finished;
+   logic [4:0] data_in;
+
+   fcd_reg testreg(clk, enable, dset, data_in, finished);
+
+   initial begin
+      $dumpfile("outfiles/out_fcd_reg_tb.vcd");
+      $dumpvars();
+
+      #1 {clk, enable, dset, data_in} = 7'b0000111;
+      #1 {clk, dset} = 2'b11;
+      #1 clk = 0;
+      #1 {clk, enable, dset} = 3'b110;
+
+      for (int i = 1; i <= 21; i = i + 1) begin
+	 #1 clk = !clk;
+      end
+
+      #1 {clk, enable} = 2'b10;
+      #1 clk = 0;
+      #1 clk = 1;
+
+      for (int i = 1; i <= 5; i = i + 1) begin
+	 #1 clk = !clk;
+      end
+      
+      #1 {clk, enable, dset} = 3'b111;
+      
+      for (int i = 1; i <= 5; i = i + 1) begin
+	 #1 clk = !clk;
+      end
+
+      #1 $finish;
+   end // initial begin
+   
+endmodule // fcd_reg_tb
+
+
+
+
+
+`endif //  `ifndef _no_testbench_
+`endif //  `ifndef _fcd_reg_v_28_
+
